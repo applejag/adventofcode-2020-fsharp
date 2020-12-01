f4bccc64dce2590be431026edde9f4b8d84e2575
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-01 22:33:49 +0100

    Completed day 1, part 2

--- src/Day01/Program.fs
+++ src/Day01/Program.fs
@@ -5,23 +5,41 @@ let loadIntegers filename =
     |> Array.map int
 
 let findPairWithSum sum ints =
-    let map = Set.ofArray ints
+    let intsSet = Set.ofArray ints
     ints
-    |> Array.tryFind (fun i -> map |> Set.contains (sum - i))
+    |> Array.tryFind (fun i -> intsSet |> Set.contains (sum - i))
     |> function
-        | Some i -> Some (i, sum - i)
+        | Some i -> Some [ i; sum - i ]
         | None -> None
+
+let findTripletWithSum sum ints =
+    let createSetWithValueAdded skip value =
+        (value, Seq.skip skip ints |> Seq.map ((+) value) |> Set.ofSeq)
+
+    let sets = ints |> Array.mapi createSetWithValueAdded
+
+    ints
+    |> Array.tryPick (fun i2 ->
+        sets
+        |> Array.tryPick (fun (i, s) ->
+            if i <> i2 && Set.contains (sum - i2) s then
+                Some [ i; i2; sum - i2 - i ]
+            else
+                None
+        )
+    )
 
 [<EntryPoint>]
 let main _ =
     let ints = loadIntegers "./input.txt"
     printfn "Loaded %i numbers" ints.Length
 
-    let pair = findPairWithSum 2020 ints
-    printfn "Pair: %A" pair
+    let result = findTripletWithSum 2020 ints
+    printfn "Result: %A" result
 
-    match pair with
-    | Some (x, y) -> printfn "Product: %i" (x * y)
+    match result with
+    | Some values ->
+        printfn "Sum: %i" <| List.reduce (+) values
+        printfn "Product: %i" <| List.reduce (*) values
     | None -> ()
     0
-

e4f1d02415c98cae33d9956f9f64316b0b7a130e
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-02 07:04:09 +0100

    Day 2, part 1

--- src/Day02/Program.fs
+++ src/Day02/Program.fs
@@ -1,13 +1,50 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
-open System
+open System.IO
+open System.Text.RegularExpressions
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+type Password = string
+
+type Policy = {
+    Char: char
+    Min: int
+    Max: int
+}
+
+let fallsWithinRange min max num =
+    num >= min && num <= max
+
+let validatePassword policy (pwd: Password) =
+    pwd
+    |> Seq.filter ((=) policy.Char)
+    |> Seq.length
+    |> fallsWithinRange policy.Min policy.Max
+
+type PolicydPassword(policy, password) =
+    member _.IsValid: bool = validatePassword policy password
+
+let pwdRegex = Regex(@"(\d+)-(\d+) (\w): (.*)", RegexOptions.Compiled)
+
+let parsePassword line =
+    let m = pwdRegex.Match line
+    if m.Success then
+        let pwd = m.Groups.[4].Value
+        let policy = {
+            Char = m.Groups.[3].Value.[0]
+            Min = int m.Groups.[1].Value
+            Max = int m.Groups.[2].Value
+        }
+        Some <| PolicydPassword(policy, pwd)
+    else
+        None
+
+let readPasswords filename =
+    File.ReadAllLines filename
+    |> Array.Parallel.choose parsePassword
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    let pwds = readPasswords "./input.txt"
+    printfn "Found %i passwords" pwds.Length
+    printfn "Found %i valid passwords" (pwds |> Seq.filter (fun pwd -> pwd.IsValid) |> Seq.length)
     0 // return an integer exit code

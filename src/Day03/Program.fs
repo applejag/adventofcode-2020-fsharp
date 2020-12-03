170a6adf0a07300344679b1568df6bdbfc6f7445
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-03 07:02:05 +0100

    Parsing the grid

--- src/Day03/Program.fs
+++ src/Day03/Program.fs
@@ -1,13 +1,37 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+open System.IO
 
-open System
+type Node =
+    | Tree
+    | Empty
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+type Map = Node[,]
+
+let readLines filename =
+    File.ReadAllLines filename
+
+let parseChar = function
+    | '#' -> Tree
+    | '.' -> Empty
+    | c -> raise (System.Exception(sprintf "Invalid char '%c'" c))
+
+let parseLine (line: string) =
+    line.ToCharArray()
+    |> Array.map parseChar
+
+let jaggedTo2D (jagged: 'a[][]) =
+    let w = jagged.[0].Length
+    let h = jagged.Length
+    let init x y = jagged.[y].[x]
+
+    Array2D.init w h init
 
 [<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+let main _ =
+    let lines = readLines "./input.txt"
+    printfn "Read %i lines" lines.Length
+
+    let jagged = lines |> Array.map parseLine
+    let grid = jaggedTo2D jagged
+    printfn "Parsed %ix%i grid" (Array2D.length1 grid) (Array2D.length2 grid)
+
+    0

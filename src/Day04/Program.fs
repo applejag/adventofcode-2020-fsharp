ab33e2fde0a2b0b52a49c05c4b7d395dadedc1ba
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-04 21:38:25 +0100

    Done with day 4, part 2

--- src/Day04/Program.fs
+++ src/Day04/Program.fs
@@ -42,7 +42,7 @@ let parsePassportOfMap (map: Map<string,string>) =
         cid = tryFind "cid"
     }
 
-let validatePassport passport =
+let validatePassport1 passport =
     passport.byr <> None
     && passport.iyr <> None
     && passport.eyr <> None
@@ -51,6 +51,61 @@ let validatePassport passport =
     && passport.ecl <> None
     && passport.pid <> None
     //&& passport.cid <> None
+
+let colorRegex = Regex(@"^#[0-9a-f]{6}$", RegexOptions.Compiled)
+let isValidColor = colorRegex.IsMatch
+
+let isIntInRange min max i =
+    i >= min && i <= max
+
+let isValidIntInRange min max str =
+    try
+        isIntInRange min max (int str)
+    with
+    | _ -> false
+
+let distanceRegex = Regex(@"^(\d+)(cm|in)$", RegexOptions.Compiled)
+type Distance =
+    | Centimeter of int
+    | Inches of int
+
+let isValidDistance str =
+    let matchDistance str =
+        let m = distanceRegex.Match str
+        match m.Success with
+        | true -> Some (int m.Groups.[1].Value, m.Groups.[2].Value)
+        | false -> None
+
+    match matchDistance str with
+    | Some (i, "cm") -> isIntInRange 150 193 i
+    | Some (i, "in") -> isIntInRange 59 76 i
+    | _ -> false
+
+let validEyeColors = Set.ofList [ "amb"; "blu"; "brn"; "gry"; "grn"; "hzl"; "oth" ]
+let isValidEyeColor str = Set.contains str validEyeColors
+
+let pidRegex = Regex(@"^\d{9}$", RegexOptions.Compiled)
+let isValidPid = pidRegex.IsMatch
+
+let isSomeAnd f = function
+    | None -> false
+    | Some v -> f v
+
+let validatePassport2 passport =
+    passport.byr |> isSomeAnd (isValidIntInRange 1920 2002)
+    && passport.iyr |> isSomeAnd (isValidIntInRange 2010 2020)
+    && passport.eyr |> isSomeAnd (isValidIntInRange 2020 2030)
+    && passport.hgt |> isSomeAnd isValidDistance
+    && passport.hcl |> isSomeAnd isValidColor
+    && passport.ecl |> isSomeAnd isValidEyeColor
+    && passport.pid |> isSomeAnd isValidPid
+
+module Array =
+    module Parallel =
+        let count predicate arr =
+            Array.Parallel.map predicate arr
+            |> Seq.filter ((=) true)
+            |> Seq.length
 
 [<EntryPoint>]
 let main _ =
@@ -65,10 +120,16 @@ let main _ =
         |> Array.Parallel.map parsePassportMapOfLines
         |> Array.Parallel.map parsePassportOfMap
 
-    let validPassports =
-        passports
-        |> Array.filter validatePassport
+    printfn ""
+    printfn "Part 1"
+    passports
+    |> Array.Parallel.count validatePassport1
+    |> printfn " Found %i valid passports"
 
-    printfn "Found %i valid passports" validPassports.Length
+    printfn ""
+    printfn "Part 2"
+    passports
+    |> Array.Parallel.count validatePassport2
+    |> printfn " Found %i valid passports"
 
     0

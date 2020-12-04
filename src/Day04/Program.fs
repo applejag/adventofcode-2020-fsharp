ab25077af7b0ad3fbd36284f05365264c0ad3bd9
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-04 08:25:13 +0100

    Day 4, part 1 done

--- src/Day04/Program.fs
+++ src/Day04/Program.fs
@@ -1,13 +1,74 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+open System.IO
+open System.Text.RegularExpressions
 
-open System
+type Passport = {
+    byr: string option // Birth Year
+    iyr: string option // Issue Year
+    eyr: string option // Expiration Year
+    hgt: string option // Height
+    hcl: string option // Hair Color
+    ecl: string option // Eye Color
+    pid: string option // Passport ID
+    cid: string option // Country ID
+}
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let readLines filename = File.ReadAllLines filename
+
+let splitPassportSections lines =
+    Array.fold (fun grouped line ->
+        match line with
+        | "" -> [] :: grouped
+        | line -> (line :: grouped.Head) :: grouped.Tail
+    ) [[]] lines
+
+let passportKeyValueRegex = Regex(@"(\w+):([^ ]+)", RegexOptions.Compiled)
+
+let parsePassportMapOfLines lines =
+    lines
+    |> Seq.collect passportKeyValueRegex.Matches
+    |> Seq.map (fun m -> (m.Groups.[1].Value, m.Groups.[2].Value))
+    |> Map.ofSeq
+
+let parsePassportOfMap (map: Map<string,string>) =
+    let tryFind key = Map.tryFind key map
+    {
+        byr = tryFind "byr"
+        iyr = tryFind "iyr"
+        eyr = tryFind "eyr"
+        hgt = tryFind "hgt"
+        hcl = tryFind "hcl"
+        ecl = tryFind "ecl"
+        pid = tryFind "pid"
+        cid = tryFind "cid"
+    }
+
+let validatePassport passport =
+    passport.byr <> None
+    && passport.iyr <> None
+    && passport.eyr <> None
+    && passport.hgt <> None
+    && passport.hcl <> None
+    && passport.ecl <> None
+    && passport.pid <> None
+    //&& passport.cid <> None
 
 [<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+let main _ =
+    let lines = readLines "./input.txt"
+    printfn "Read %i lines" lines.Length
+
+    let passportSections = splitPassportSections lines |> Array.ofList
+    printfn "Found %i passports" passportSections.Length
+
+    let passports =
+        passportSections
+        |> Array.Parallel.map parsePassportMapOfLines
+        |> Array.Parallel.map parsePassportOfMap
+
+    let validPassports =
+        passports
+        |> Array.filter validatePassport
+
+    printfn "Found %i valid passports" validPassports.Length
+
+    0

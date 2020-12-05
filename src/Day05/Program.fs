d703bbc4950618f1b53ed8b4c73ddd1654c423c4
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-05 07:24:52 +0100

    Completed day 5 part 2

--- src/Day05/Program.fs
+++ src/Day05/Program.fs
@@ -66,12 +66,12 @@ let (|SinglePoint|_|) = function
     | { X = (x1,x2); Y = (y1,y2) } when x1=x2 && y1=y2 -> Some (x1, y1)
     | _ -> None
 
-let calculatePoint dirs =
+let calculateSeatPos dirs =
     dirs
     |> Array.fold partition { X = (0,7); Y = (0,127) }
     |> function
         | SinglePoint point -> Ok point
-        | rect -> Error (sprintf "Dirs '%A' did not resolve to a single point: %A" dirs rect)
+        | rect -> Error (sprintf "Dirs did not resolve to a single point: %A" rect)
 
 let calculateSeadId = function
     | x, y -> x + y * 8
@@ -80,17 +80,57 @@ let resultToOption = function
     | Ok v -> Some v
     | Error _ -> None
 
+let printSeats seatIds =
+    let setOfSeatIds = Set.ofArray seatIds
+    let highestSeatId = setOfSeatIds.MaximumElement
+
+    seq { 0..highestSeatId }
+    |> Seq.iter (fun seatId ->
+        if seatId % 8 = 0 then
+            printf "\n%3i: " (seatId / 8)
+
+        let c =
+            match Set.contains seatId setOfSeatIds with
+            | true -> 'x' | false -> '.'
+
+        printf "%c" c
+    )
+
+    printfn ""
+
 [<EntryPoint>]
 let main _ =
     let lines = readLines "./input.txt"
     printfn "Read %i lines" lines.Length
 
-    let parsedLines = Array.Parallel.map parseLine lines
-    let pointsResults = Array.Parallel.map calculatePoint parsedLines
-    let points = pointsResults |> Array.choose resultToOption
+    let seats =
+        lines
+        |> Array.Parallel.mapi (fun i line ->
+            parseLine line
+            |> calculateSeatPos
+            |> function
+                | Ok v -> v
+                | Error err -> failwithf "Failed at %i:%s: %s" i line err
+        )
 
-    printfn "Found %i/%i points from all seats definitions" points.Length pointsResults.Length
-    let highestSeatId = points |> Array.Parallel.map calculateSeadId |> Array.max
+    printfn "Found %i points from all seats definitions" seats.Length
+    let seatIds = seats |> Array.Parallel.map calculateSeadId
+    let highestSeatId = Array.max seatIds
     printfn "Highest sead ID: %i" highestSeatId
+
+    let setOfSeatIds = Set.ofArray seatIds
+    let lowestSeatId = Array.min seatIds
+
+    printfn ""
+    printfn "Part 2:"
+    seq { lowestSeatId..highestSeatId }
+    |> Seq.choose (fun seatId ->
+        if Set.contains seatId setOfSeatIds then
+            None
+        else
+            Some seatId
+    )
+    |> Seq.iter (printfn " Unoccupied seat ID: %i")
+    printfn "Done."
 
     0

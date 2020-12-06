f791779329a7402be04db578990b17920dbdde8a
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-06 12:03:11 +0100

    Done with day 6 part 1

--- src/Day06/Program.fs
+++ src/Day06/Program.fs
@@ -1,13 +1,32 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+open System.IO
 
-open System
+let readLines filename = File.ReadAllLines filename
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let splitIntoGroups lines =
+    Array.fold (fun grouped line ->
+        match line with
+        | "" -> [] :: grouped
+        | line -> (line :: grouped.Head) :: grouped.Tail
+    ) [[]] lines
+
+let foldListToSet charsList =
+    charsList
+    |> List.map Set
+    |> List.fold Set.union Set.empty
 
 [<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+let main _ =
+    let lines = readLines "./input.txt"
+    printfn "Read %i lines" lines.Length
+
+    let groups = splitIntoGroups lines |> Array.ofList
+    printfn "Found %i groups" groups.Length
+
+    let answersPerGroups = Array.map foldListToSet groups
+    let sumOfAnswersPerGroups =
+        answersPerGroups
+        |> Array.sumBy Set.count
+
+    printfn "Sum of all groups answers: %i" sumOfAnswersPerGroups
+
     0 // return an integer exit code

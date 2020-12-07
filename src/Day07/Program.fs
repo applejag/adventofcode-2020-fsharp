0794d7327e9f6e6b5ee1c0f10e889ee4022643f0
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-07 10:37:35 +0100

    Done with day 7, part 1

--- src/Day07/Program.fs
+++ src/Day07/Program.fs
@@ -1,13 +1,75 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+open System.IO
+open System.Text.RegularExpressions
 
-open System
+let readLines filename =
+    File.ReadAllLines filename
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+/// Example: dim tan bags contain 3 shiny teal bags, 5 bright white bags, 4 striped bronze bags.
+/// => { BagColor = "dim tan"; Holds = map [("shiny teal", 3); ("bright white",5); ("striped bronze"; 4)]
+type LuggageRule = {
+    BagColor: string
+    Holds: Map<string, int>
+}
+
+let luggageRuleRegex = Regex(@"(^|\d+ )(.*?) bags?", RegexOptions.Compiled)
+let parseLuggageRule line =
+    let matches = luggageRuleRegex.Matches line
+    {
+        BagColor = (Seq.item 0 matches).Groups.[2].Value
+        Holds = matches
+            |> Seq.skip 1
+            |> Seq.map (fun m -> m.Groups.[2].Value, int m.Groups.[1].Value)
+            |> Map.ofSeq
+    }
+
+let addToMapOfSets key item map =
+    match Map.tryFind key map with
+    | Some items -> map |> Map.add key (Set.add item items)
+    | None -> map |> Map.add key (Set [item])
+
+let calcParentalMappings rules =
+    rules
+    |> Array.fold (fun map rule ->
+        Map.toSeq rule.Holds
+        |> Seq.map fst
+        |> Seq.fold (fun map bag -> map |> addToMapOfSets bag rule.BagColor) map
+    ) Map.empty
+
+let findBagsThatEventuallyContains soughtColor parentalMap =
+    let rec find map depth col =
+        (* These `printfn` calls will draw a tree structure of the findings *)
+        //printfn "%s%s" (System.String(' ', depth*2)) col
+        let x = match Map.tryFind col map with
+                | None -> Set [col]
+                | Some bags ->
+                    bags
+                    |> Seq.map (find map (depth+1))
+                    |> Seq.reduce Set.union
+                    |> Set.add col
+        //printfn "%s| %i" (System.String(' ', depth*2)) x.Count
+        x
+
+    find parentalMap 1 soughtColor
+    |> Set.remove soughtColor
 
 [<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+let main _ =
+    let lines = readLines "./input.txt"
+    printfn "Read %i lines" lines.Length
+
+    let rules = Array.Parallel.map parseLuggageRule lines
+    printfn "Parsed rules for %i different bag colors" (rules |> Array.distinctBy (fun r -> r.BagColor) |> Array.length)
+
+    let parentalMap = calcParentalMappings rules
+    //parentalMap
+    //|> Map.iter (printfn " %20s is found in %O")
+
+    let soughtColor = "shiny gold"
+    let bags = findBagsThatEventuallyContains soughtColor parentalMap
+    printfn "Found %i bags that eventually contain '%s':" bags.Count soughtColor
+
+    //let firstWordRegex = Regex(@"^\w+", RegexOptions.Compiled)
+    //bags
+    //|> Seq.groupBy (fun col -> (firstWordRegex.Match col).Value)
+    //|> Seq.iter (fun (g, cols) -> printfn "%s %i:\n  %s" g (Seq.length cols) <| String.concat ", " cols)
+    0

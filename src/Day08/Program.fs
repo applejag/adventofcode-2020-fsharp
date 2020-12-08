4a1094e046ee50c2baf508463002658a5e671d5c
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-08 14:02:55 +0100

    Done with day 8, part 2

--- src/Day08/Program.fs
+++ src/Day08/Program.fs
@@ -4,10 +4,10 @@ open System.Runtime.CompilerServices
 let readLines filename =
     File.ReadAllLines filename
 
-type Operation =
-    | Nop of int
-    | Acc of int
-    | Jmp of int
+type OpCode =
+    | Nop
+    | Acc
+    | Jmp
 
 let splitOp (line: string) =
     // Using String.item[Range] is more forgiving than String.Substring
@@ -16,9 +16,9 @@ let splitOp (line: string) =
 
 let parseOp line =
     match splitOp line with
-    | "nop", nop -> Nop (int nop)
-    | "acc", acc -> Acc (int acc)
-    | "jmp", jmp -> Jmp (int jmp)
+    | "nop", nop -> Nop, int nop
+    | "acc", acc -> Acc, int acc
+    | "jmp", jmp -> Jmp, int jmp
     | _ -> failwithf "Unable to parse op: '%s'" line
 
 [<IsReadOnly; Struct>]
@@ -30,9 +30,9 @@ type ProgramState(pc: int, acc: int) =
 
 let applyOp (state: ProgramState) op =
     match op with
-    | Nop _ -> ProgramState(state.Pc + 1, state.Acc)
-    | Acc acc -> ProgramState(state.Pc + 1, state.Acc + acc)
-    | Jmp jmp -> ProgramState(state.Pc + jmp, state.Acc)
+    | Nop, _ -> ProgramState(state.Pc + 1, state.Acc)
+    | Acc, acc -> ProgramState(state.Pc + 1, state.Acc + acc)
+    | Jmp, jmp -> ProgramState(state.Pc + jmp, state.Acc)
 
 let pair v = (v, v)
 
@@ -42,28 +42,62 @@ let walkProgram ops =
         |> Option.map (applyOp state >> pair)
     ) ProgramState.Empty
 
-type Continuation<'State> =
-    | Continue
-    | Next of 'State
-
 module Seq =
-    let foldFind chooser (state: 'State) (source: seq<'T>) =
-        let mutable s = state
+    let foldWhileSome folder state source =
         source
-        |> Seq.tryPick (fun item ->
-            match chooser s item with
-            | Continue -> Some item
-            | Next state -> s <- state; None
-        )
+        |> Seq.scan (fun prevState item ->
+            folder (Option.get prevState) item
+        ) (Some state)
+        |> Seq.takeWhile Option.isSome
+        |> Seq.map Option.get
+        |> Seq.last
 
-let stopWhenLoopDetected (states: seq<ProgramState>) =
-    states
-    |> Seq.foldFind (fun pcs state ->
-        if Set.contains state.Pc pcs then
-            Continue
-        else
-            Next (Set.add state.Pc pcs)
-    ) Set.empty
+type RunState =
+    | NotStarted
+    | Running
+    | InfiniteLoopDetected
+    | RanToCompletetion
+
+let stopWhenLoopDetected states =
+    let _, lastState, stoppedReason =
+        states
+        |> Seq.foldWhileSome (fun (pcs, _, lastRunState) (state: ProgramState) ->
+            match lastRunState with
+            | NotStarted
+            | Running ->
+                let newRunState = if Set.contains state.Pc pcs then InfiniteLoopDetected else Running
+                Some (Set.add state.Pc pcs, state, newRunState)
+            | _ -> None
+        ) (Set.empty, ProgramState(), NotStarted)
+    match stoppedReason with
+    | Running -> lastState, RanToCompletetion
+    | _ -> lastState, stoppedReason
+
+let withOneOpChanged replaceOp withOp ops =
+    ops
+    |> Seq.indexed
+    |> Seq.filter (fun (_, (op, _)) -> op = replaceOp)
+    |> Seq.map (fun (i, (_, opi)) ->
+        let alteredOps = Array.copy ops
+        alteredOps.[i] <- withOp, opi
+        i, alteredOps
+    )
+
+let runAndPrintWithOneOpChanged replaceOp withOp ops =
+    let completions =
+        ops
+        |> withOneOpChanged replaceOp withOp
+        |> Seq.map (fun (i, ops) ->
+            let finalState, stoppedReason = walkProgram ops |> stopWhenLoopDetected
+            (i, finalState, stoppedReason)
+        )
+        |> Seq.filter (fun (_, _, stoppedReason) -> stoppedReason = RanToCompletetion)
+        |> Array.ofSeq
+    printfn " With %A->%A, %i attempts succeeded." replaceOp withOp completions.Length
+    completions
+    |> Array.iter (fun (i, finalState, _) ->
+        printfn "  ops.[%i] <- %A succeeded with ending state: %A" i withOp finalState
+    )
 
 [<EntryPoint>]
 let main _ =
@@ -72,10 +106,17 @@ let main _ =
 
     let ops = Array.Parallel.map parseOp lines
     printfn "Parsed %i ops: %A" ops.Length ops
-    printfn ""
 
+    printfn ""
+    printfn "Part 1:"
     let statesSeq = walkProgram ops
-    let loopDetectedAt = stopWhenLoopDetected statesSeq
-    printfn "Detected loop at: %A" loopDetectedAt
+    let finalState, stoppedReason = stopWhenLoopDetected statesSeq
+    printfn " Last state: %A" finalState
+    printfn " Stopped reason: %A" stoppedReason
+
+    printfn ""
+    printfn "Part 2:"
+    runAndPrintWithOneOpChanged Nop Jmp ops
+    runAndPrintWithOneOpChanged Jmp Nop ops
 
     0

dd8d9c2b02bfaa2d0a7e6c1ebc1e9a54d83c1b4b
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-08 12:16:54 +0100

    Done with day 8, part 1

--- src/Day08/Program.fs
+++ src/Day08/Program.fs
@@ -1,13 +1,77 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+open System.IO
+open System.Runtime.CompilerServices
 
-open System
+let readLines filename =
+    File.ReadAllLines filename
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+type Operation =
+    | Nop
+    | Acc of int
+    | Jmp of int
+
+let splitOp (line: string) =
+    // Using String.item[Range] is more forgiving than String.Substring
+    // as it returns empty string when indices are out of bounds
+    line.[0..2], line.[4..]
+
+let parseOp line =
+    match splitOp line with
+    | "nop", _ -> Nop
+    | "acc", acc -> Acc (int acc)
+    | "jmp", jmp -> Jmp (int jmp)
+    | _ -> failwithf "Unable to parse op: '%s'" line
+
+[<IsReadOnly; Struct>]
+type ProgramState(pc: int, acc: int) =
+    member _.Pc = pc
+    member _.Acc = acc
+    static member Empty = ProgramState(0,0)
+    override this.ToString() = sprintf "{ Pc = %i; Acc = %i }" this.Pc this.Acc
+
+let applyOp (state: ProgramState) op =
+    match op with
+    | Nop -> ProgramState(state.Pc + 1, state.Acc)
+    | Acc acc -> ProgramState(state.Pc + 1, state.Acc + acc)
+    | Jmp jmp -> ProgramState(state.Pc + jmp, state.Acc)
+
+let pair v = (v, v)
+
+let walkProgram ops =
+    Seq.unfold (fun (state: ProgramState) ->
+        Array.tryItem state.Pc ops
+        |> Option.map (applyOp state >> pair)
+    ) ProgramState.Empty
+
+module Seq =
+    let foldFind chooser (state: 'State) (source: seq<'T>) =
+        let mutable s = state
+        source
+        |> Seq.tryPick (fun item ->
+            match chooser s item with
+            | None -> Some item
+            | Some state -> s <- state; None
+        )
+
+let stopWhenLoopDetected (states: seq<ProgramState>) =
+    states
+    |> Seq.foldFind (fun pcs state ->
+        if Set.contains state.Pc pcs then
+            None
+        else
+            Some (Set.add state.Pc pcs)
+    ) Set.empty
 
 [<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+let main _ =
+    let lines = readLines "./input.txt"
+    printfn "Read %i lines" lines.Length
+
+    let ops = Array.Parallel.map parseOp lines
+    printfn "Parsed %i ops: %A" ops.Length ops
+    printfn ""
+
+    let statesSeq = walkProgram ops
+    let loopDetectedAt = stopWhenLoopDetected statesSeq
+    printfn "Detected loop at: %A" loopDetectedAt
+
+    0

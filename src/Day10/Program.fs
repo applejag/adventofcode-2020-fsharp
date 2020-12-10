d49d660677cda45601a8a75853c072ff7b575724
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-10 09:09:13 +0100

    Extracted differences calc into seperate func

--- src/Day10/Program.fs
+++ src/Day10/Program.fs
@@ -3,6 +3,14 @@ open System.IO
 let readLines filename =
     File.ReadAllLines filename
 
+let calcAndPrintDifferences joltages =
+    let differences = joltages |> Array.pairwise |> Array.map (fun (a, b) -> b - a)
+    let countPerDiff = differences |> Array.countBy id |> Map.ofArray
+    printfn " Differences: %A" countPerDiff
+    let numOf1Diffs = countPerDiff.[1]
+    let numOf3Diffs = countPerDiff.[3]
+    printfn " 1 diffs * 3 diffs = %i" (numOf1Diffs * numOf3Diffs)
+
 [<EntryPoint>]
 let main args =
     let filename = Array.tryItem 0 args |> Option.defaultValue "./input.txt"
@@ -11,15 +19,10 @@ let main args =
 
     let adapters = lines |> Array.map int
     let highest = adapters |> Array.max
-
     let joltages = adapters |> Array.append [| 0; highest + 3 |] |> Array.sort
-    let differences = joltages |> Array.pairwise |> Array.map (fun (a, b) -> b - a)
-    let countPerDiff = differences |> Array.countBy id |> Map.ofArray
 
-    printfn "Differences: %A" countPerDiff
-    let numOf1Diffs = countPerDiff.[1]
-    let numOf3Diffs = countPerDiff.[3]
-
-    printfn "1 diffs * 3 diffs = %i" (numOf1Diffs * numOf3Diffs)
+    printfn ""
+    printfn "Part 1:"
+    calcAndPrintDifferences joltages
 
     0

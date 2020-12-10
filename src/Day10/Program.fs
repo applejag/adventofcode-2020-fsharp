70cd4350d6061915bbd6faa06a404714dd5fed0f
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-10 08:33:33 +0100

    Prepared day10 template

--- src/Day10/Program.fs
+++ src/Day10/Program.fs
@@ -1,13 +1,11 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+open System.IO
 
-open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let readLines filename =
+    File.ReadAllLines filename
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    let lines = readLines "./input.txt"
+    printfn "Read %i lines" lines.Length
+
+    0

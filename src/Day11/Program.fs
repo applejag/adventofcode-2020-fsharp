0131b9110d18a4e50b1515e46c916899d46787f9
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-12-11 12:54:40 +0100

    Done with day 11, part 2

--- src/Day11/Program.fs
+++ src/Day11/Program.fs
@@ -15,7 +15,7 @@ let convertLinesTo2DArr lines =
     Array.map Array.ofSeq lines
     |> jaggedTo2D
 
-let adjasentIndices = [|
+let adjasentDirections = [|
     (-1,-1); (0,-1); (1,-1);
     (-1,0);  (*0,0*) (1,0);
     (-1,1);  (0,1);  (1,1);
@@ -40,7 +40,7 @@ module Array2D =
         }
 
 let countAdjasentSeats c x y (grid: char[,]) =
-    adjasentIndices
+    adjasentDirections
     |> Seq.map (fun (dx, dy) -> Array2D.tryItem (x+dx) (y+dy) grid)
     |> Seq.filter ((=) (Some c))
     |> Seq.length
@@ -53,9 +53,9 @@ let countSeats c (grid: char[,]) =
 let evolveGrid seatEvolver grid =
     grid |> Array2D.mapi (seatEvolver grid)
 
-type OccupiedSeats = {
+type EquilibriumResult = {
     Iteration: int
-    Count: int
+    Occupied: int
 }
 
 let evolveUntilEquilibrium seatEvolver grid =
@@ -66,13 +66,49 @@ let evolveUntilEquilibrium seatEvolver grid =
         else
             Some (occupied, (occupied, evolveGrid seatEvolver g))
     ) (-1, grid)
-    |> Seq.mapi (fun i count -> { Iteration = i; Count = count })
+    |> Seq.mapi (fun i count ->
+        if i >= 10000 then
+            failwithf "Reached %i iterations. May be infinite loop... Aborting." i
+        else
+            { Iteration = i; Occupied = count }
+    )
     |> Seq.last
 
 let evolveSeat1 grid x y c =
     match c with
     | 'L' when countAdjasentSeats '#' x y grid = 0 -> '#'
     | '#' when countAdjasentSeats '#' x y grid >= 4 -> 'L'
+    | _ -> c
+
+let createRay grid x y dx dy =
+    let size = max (Array2D.length1 grid) (Array2D.length2 grid)
+    // +1 on i to exclude pos (x,y) from the ray
+    let xs = Seq.init size (fun i -> x + dx * (i + 1))
+    let ys = Seq.init size (fun i -> y + dy * (i + 1))
+    Seq.zip xs ys
+
+let isSeat c = c = '#' || c = 'L'
+
+let raycastSeats grid ray =
+    ray
+    |> Seq.tryPick (fun (x, y) ->
+        Array2D.tryItem x y grid
+        |> Option.filter isSeat
+    )
+
+let countRaycastedSeats c x y grid =
+    adjasentDirections
+    |> Seq.map (fun (dx, dy) ->
+        createRay grid x y dx dy
+        |> raycastSeats grid
+    )
+    |> Seq.filter ((=) (Some c))
+    |> Seq.length
+
+let evolveSeat2 grid x y c =
+    match c with
+    | 'L' when countRaycastedSeats '#' x y grid = 0 -> '#'
+    | '#' when countRaycastedSeats '#' x y grid >= 5 -> 'L'
     | _ -> c
 
 //let printGrid grid =
@@ -97,8 +133,11 @@ let main args =
     printfn ""
     printfn "Part 1:"
     let equilibrium1 = evolveUntilEquilibrium evolveSeat1 grid
-    printfn " Iteration %3i: %5i seats occupied" equilibrium1.Iteration equilibrium1.Count
+    printfn " Iteration %3i: %5i seats occupied" equilibrium1.Iteration equilibrium1.Occupied
 
     printfn ""
     printfn "Part 2:"
+    let equilibrium2 = evolveUntilEquilibrium evolveSeat2 grid
+    printfn " Iteration %3i: %5i seats occupied" equilibrium2.Iteration equilibrium2.Occupied
+
     0
